03de4addb128d47a13d81e6d5d6887d6afb73a2c
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-07-19 20:03:16 +0900

    create dynamodb table

---
+++ dynamoDB/create_dynamodb_tables_json.py
@@ -0,0 +1,40 @@
+import datetime
+import json
+import os
+import sys
+import time
+import boto3
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from common.logger import ColerdLog
+
+if __name__ == "__main__":
+    lg = ColerdLog.init_logger(__name__)
+    lg.info('**** Delete All DynamoDB Tables ****')
+    session = boto3.Session(profile_name='yoshiko')
+    client = session.client('dynamodb', region_name='us-east-2')
+    table_names = client.list_tables()['TableNames']
+    if table_names == []:
+        lg.info('No tables')
+    json_open = open('dynamoDB/config/create_tables.json', 'r')
+    json_data = json.load(json_open)
+    for table in json_data['tables']:
+        if "LocalSecondaryIndexes" in table:
+            result = client.create_table(
+                TableName=table["TableName"],
+                AttributeDefinitions=table["AttributeDefinitions"],
+                KeySchema=table["KeySchema"],
+                LocalSecondaryIndexes=table["LocalSecondaryIndexes"],
+                ProvisionedThroughput=table["ProvisionedThroughput"]
+            )
+        else:
+            result = client.create_table(
+                TableName=table["TableName"],
+                AttributeDefinitions=table["AttributeDefinitions"],
+                KeySchema=table["KeySchema"],
+                ProvisionedThroughput=table["ProvisionedThroughput"]
+            )
+        client.get_waiter('table_exists').wait(TableName=table["TableName"])
+        lg.info(table["TableName"] + ' has been created.')
+    
+    lg.info('**** End Delete All DynamoDB Tables ****')
+
