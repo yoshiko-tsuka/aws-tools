f0d17367d487f10b1358411327b6b3580c4b5938
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-07-21 18:51:13 +0900

    add next key for search

--- dynamoDB/search_dynamodb_tables_json.py
+++ dynamoDB/search_dynamodb_tables_json.py
@@ -12,27 +12,32 @@ if __name__ == "__main__":
     lg.info('**** Search DynamoDB Tables ****')
     session = boto3.Session(profile_name='yoshiko')
     client = session.client('dynamodb', region_name='us-east-2')
-    result = []
     table_names = client.list_tables()['TableNames']
     if table_names == []:
         lg.info('No tables')
     json_open = open('dynamoDB/config/query.json', 'r')
     json_data = json.load(json_open)
-    response = self.__dynamo_query(area, weeks, visa, timetable, school)
-        if not key in response:
-            return None
+    response = client.query(
+        KeyConditionExpression=Key('area').eq(area),
+        FilterExpression=f_exp,
+        )
+    if not key in response:
+        return None
     result.extend(response[key])
-        if "LastEvaluatedKey" in response:
-            nextKey = response["LastEvaluatedKey"]
-        while nextKey:
-            query_result = self.__dynamo_query(
-                area, weeks, visa, timetable, school, nextKey)
-            result.extend(query_result[key])
-            if not "LastEvaluatedKey" in query_result:
-                break
-            else:
-                nextKey = query_result["LastEvaluatedKey"]
-        lg.info('Put ' + table["TableName"] + ' Items has been succeeded.')
+    if "LastEvaluatedKey" in response:
+        nextKey = response["LastEvaluatedKey"]
+    while nextKey:
+        query_result = client.query(
+            KeyConditionExpression=Key('area').eq(area),
+            FilterExpression=f_exp,
+            ExclusiveStartKey=nextKey
+        )
+        result.extend(query_result[key])
+        if not "LastEvaluatedKey" in query_result:
+            break
+        else:
+            nextKey = query_result["LastEvaluatedKey"]
+    lg.info('Search ' + table["TableName"] + ' Items has been succeeded.')
     
     lg.info('**** End Search DynamoDB Tables ****')
 
