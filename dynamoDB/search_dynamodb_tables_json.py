4713b00de2669e7681f0ec651c2a989ccc9bdba8
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-07-28 18:32:30 +0900

    add query.json

--- dynamoDB/search_dynamodb_tables_json.py
+++ dynamoDB/search_dynamodb_tables_json.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from common.logger import ColerdLog
 
 if __name__ == "__main__":
+    result = []
     lg = ColerdLog.init_logger(__name__)
     lg.info('**** Search DynamoDB Tables ****')
     session = boto3.Session(profile_name='yoshiko')
@@ -18,26 +19,31 @@ if __name__ == "__main__":
     json_open = open('dynamoDB/config/query.json', 'r')
     json_data = json.load(json_open)
     response = client.query(
-        KeyConditionExpression=Key('area').eq(area),
-        FilterExpression=f_exp,
+        TableName=json_data['TableName'],
+        KeyConditionExpression=json_data['Query'],
+        ExpressionAttributeValues=json_data['Attribute'],
+        FilterExpression=json_data['Filter'],
+        ProjectionExpression=json_data['Projection']
         )
-    if not key in response:
-        return None
-    result.extend(response[key])
+    result.extend(response['Items'])
     if "LastEvaluatedKey" in response:
         nextKey = response["LastEvaluatedKey"]
-    while nextKey:
-        query_result = client.query(
-            KeyConditionExpression=Key('area').eq(area),
-            FilterExpression=f_exp,
-            ExclusiveStartKey=nextKey
-        )
-        result.extend(query_result[key])
-        if not "LastEvaluatedKey" in query_result:
-            break
-        else:
-            nextKey = query_result["LastEvaluatedKey"]
-    lg.info('Search ' + table["TableName"] + ' Items has been succeeded.')
+        while nextKey:
+            query_result = client.query(
+                TableName=json_data['TableName'],
+                KeyConditionExpression=json_data['Query'],
+                ExpressionAttributeValues=json_data['Attribute'],
+                FilterExpression=json_data['Filter'],
+                ProjectionExpression=json_data['Projection'],
+                ExclusiveStartKey=nextKey
+            )
+            result.extend(query_result["Items"])
+            if not "LastEvaluatedKey" in query_result:
+                break
+            else:
+                nextKey = query_result["LastEvaluatedKey"]
+    lg.info(result)
+    lg.info('Search ' + json_data['TableName'] + ' Items has been succeeded.')
     
     lg.info('**** End Search DynamoDB Tables ****')
 
