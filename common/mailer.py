d51bc3be7690a968feef0587f30e70419ddd9b30
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-08-31 18:10:48 +0900

    add mailer function

---
+++ common/mailer.py
@@ -0,0 +1,53 @@
+import json
+import os
+import sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.header import Header
+
+def _sendMail(RECIPIENT, SUBJECT, BODY_HTML=None, BODY_TEXT=None, ATTACHMENT=None):
+    # Create a multipart/mixed parent container.
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = SUBJECT
+    msg['From'] = SENDER
+    msg['To'] = ', '.join(RECIPIENT) if type(RECIPIENT) is list else RECIPIENT
+
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+
+    # Set html body.
+    if BODY_HTML is not None:
+        # The HTML body of the email.
+        htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+        msg_body.attach(htmlpart)
+    # Set text body.
+    if BODY_TEXT is not None:
+        # The email body for recipients with non-HTML email clients.
+        textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+        msg_body.attach(textpart)
+
+    '''
+    TODO: 本処理は使われていないためコメントアウトしている.（テストも未実施の状態）
+    # Attach files.
+    if ATTACHMENT is not None:
+        msg = _addAttachment(msg, ATTACHMENT)
+    '''
+
+    # Attach the multipart/alternative child container to the multipart/mixed
+    # parent container.
+    msg.attach(msg_body)
+
+    # Send the email.
+    # Provide the contents of the email.
+    DESTINATIONS = RECIPIENT if type(RECIPIENT) is list else [RECIPIENT]
+    response = CLIENT.send_raw_email(
+        Source=SENDER,
+        Destinations=DESTINATIONS,
+        RawMessage={
+            'Data': msg.as_string(),
+        }
+    )
+
+    return response
