61f44393f8b2394d7830d0b9143d7fcd7a30b1f0
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-09-01 23:26:21 +0900

    text mail and html mail

--- common/mailer.py
+++ common/mailer.py
@@ -1,46 +1,56 @@
 import json
 import os
 import sys
+from myconst import myconst as cst
+import boto3
+# multipart: 添付ファイルを送信する時に必要
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.header import Header
 
-def _sendMail(RECIPIENT, SUBJECT, BODY_HTML=None, BODY_TEXT=None, ATTACHMENT=None):
-    # Create a multipart/mixed parent container.
-    msg = MIMEMultipart('mixed')
-    # Add subject, from and to lines.
-    msg['Subject'] = SUBJECT
-    msg['From'] = SENDER
-    msg['To'] = ', '.join(RECIPIENT) if type(RECIPIENT) is list else RECIPIENT
+CHARSET = "UTF-8"
+SESSION = boto3.Session(profile_name=cst.PROFILE_NAME)
+CLIENT = SESSION.client('ses', region_name=cst.REGION_NAME)
+SENDER_NAME = Header(f"{environment_identifier}ワールドアベニュー".encode(
+        'iso-2022-jp'), 'iso-2022-jp').encode()
+SENDER = '{SENDER_NAME} <{cst.SENDER_MAIL_ADDRESS}>'
 
-    # Create a multipart/alternative child container.
-    msg_body = MIMEMultipart('alternative')
+class Mailer:
+    def sendHtmlMail(RECIPIENT, SUBJECT: str, BODY_HTML: str, ATTACHMENT=None):
+        return return _sendMail(RECIPIENT=RECIPIENT, SUBJECT=SUBJECT, BODY_HTML=BODY_HTML, ATTACHMENT=ATTACHMENT)
+    
+    def sendTextMail(RECIPIENT, SUBJECT: str, BODY_TEXT: str, ATTACHMENT=None):
+        return _sendMail(RECIPIENT=RECIPIENT, SUBJECT=SUBJECT, BODY_TEXT=BODY_TEXT, ATTACHMENT=ATTACHMENT)
 
-    # Set html body.
-    if BODY_HTML is not None:
-        # The HTML body of the email.
-        htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
-        msg_body.attach(htmlpart)
-    # Set text body.
-    if BODY_TEXT is not None:
-        # The email body for recipients with non-HTML email clients.
-        textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
-        msg_body.attach(textpart)
+    def _sendMail(RECIPIENT, SUBJECT, BODY_HTML=None, BODY_TEXT=None, ATTACHMENT=None):
+        # header はmixed
+        msg = MIMEMultipart('mixed')
+        # Add subject, from and to lines.
+        msg['Subject'] = SUBJECT
+        msg['From'] = cst.SENDER
+        msg['To'] = ', '.join(RECIPIENT) if type(RECIPIENT) is list else RECIPIENT
 
-    # Attach the multipart/alternative child container to the multipart/mixed
-    # parent container.
-    msg.attach(msg_body)
+        # body はalternative
+        msg_body = MIMEMultipart('alternative')
 
-    # Send the email.
-    # Provide the contents of the email.
-    DESTINATIONS = RECIPIENT if type(RECIPIENT) is list else [RECIPIENT]
-    response = CLIENT.send_raw_email(
-        Source=SENDER,
-        Destinations=DESTINATIONS,
-        RawMessage={
-            'Data': msg.as_string(),
-        }
-    )
+        if BODY_HTML is not None:
+            html = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+            msg_body.attach(html)
+            
+        if BODY_TEXT is not None:
+            text = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+            msg_body.attach(text)
 
-    return response
+        msg.attach(msg_body)
+
+        DESTINATIONS = RECIPIENT if type(RECIPIENT) is list else [RECIPIENT]
+        response = CLIENT.send_raw_email(
+            Source=SENDER,
+            Destinations=DESTINATIONS,
+            RawMessage={
+                'Data': msg.as_string(),
+            }
+        )
+
+        return response
--- common/myconst.py
+++ common/myconst.py
@@ -4,3 +4,4 @@ import sys
 class myconst:
     PROFILE_NAME = 'yoshiko'
     REGION_NAME = 'us-east-2'
+    SENDER_MAIL_ADDRESS = '44534453yoshiko@gmail.com'
