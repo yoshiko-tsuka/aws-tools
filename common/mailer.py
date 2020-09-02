a51353ae22630d3fd14ae8e94b0cc46766593a89
yoshiko-tsuka <44534453yoshiko@gmail.com>
2020-09-02 20:12:31 +0900

    use boto3 send mail instead of send raw mail

--- common/mailer.py
+++ common/mailer.py
@@ -12,9 +12,7 @@ from email.header import Header
 CHARSET = "UTF-8"
 SESSION = boto3.Session(profile_name=cst.PROFILE_NAME)
 CLIENT = SESSION.client('ses', region_name=cst.REGION_NAME)
-SENDER_NAME = Header(f"{environment_identifier}ワールドアベニュー".encode(
-        'iso-2022-jp'), 'iso-2022-jp').encode()
-SENDER = '{SENDER_NAME} <{cst.SENDER_MAIL_ADDRESS}>'
+SENDER = cst.SENDER_MAIL_ADDRESS
 
 class Mailer:
     def sendHtmlMail(RECIPIENT, SUBJECT: str, BODY_HTML: str, ATTACHMENT=None):
@@ -24,33 +22,25 @@ class Mailer:
         return _sendMail(RECIPIENT=RECIPIENT, SUBJECT=SUBJECT, BODY_TEXT=BODY_TEXT, ATTACHMENT=ATTACHMENT)
 
     def _sendMail(RECIPIENT, SUBJECT, BODY_HTML=None, BODY_TEXT=None, ATTACHMENT=None):
-        # header はmixed
-        msg = MIMEMultipart('mixed')
-        # Add subject, from and to lines.
-        msg['Subject'] = SUBJECT
-        msg['From'] = cst.SENDER
-        msg['To'] = ', '.join(RECIPIENT) if type(RECIPIENT) is list else RECIPIENT
-
-        # body はalternative
-        msg_body = MIMEMultipart('alternative')
+        msg = {}
+        msg['Body'] = {}
 
         if BODY_HTML is not None:
-            html = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
-            msg_body.attach(html)
+            msg['Body']['Html']['Charset'] = CHARSET
+            msg['Body']['Html']['Data'] = BODY_HTML.encode(CHARSET)
             
         if BODY_TEXT is not None:
-            text = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
-            msg_body.attach(text)
+            msg['Body']['Text']['Charset'] = CHARSET
+            msg['Body']['Text']['Data'] = BODY_TEXT.encode(CHARSET)
 
-        msg.attach(msg_body)
+        msg['Subject']['Charset'] = SUBJECT
+        msg['Subject']['Data'] = SUBJECT
 
-        DESTINATIONS = RECIPIENT if type(RECIPIENT) is list else [RECIPIENT]
-        response = CLIENT.send_raw_email(
+        DESTINATIONS['ToAddresses'] = RECIPIENT
+        response = CLIENT.send_email(
             Source=SENDER,
             Destinations=DESTINATIONS,
-            RawMessage={
-                'Data': msg.as_string(),
-            }
+            Message=msg
         )
 
         return response
